4455d6903342345e34b895c6d37b32443acddccb
caciolai <andrea.caciolai@gmail.com>
2021-10-13 18:28:09 +0200

    Correct package structure

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(
     description='Geometric Deep Learning for Virality Prediction of Hashtags.',
     author='caciolai, crisostomi',
     license='MIT License',
-    url='https://github.com/caciolai/Semantic-Role-Labeling-exploiting-Contextualized-Embeddings',
-    packages=find_packages(where="src"),
-    python_requires='>3.7.9',
+    url='https://github.com/caciolai/Geometric-Deep-Learning-for-Virality-Prediction-of-Hashtags',
+    packages=find_packages("src"),  # include all packages under src
+    package_dir={"": "src"},   # tell distutils packages are under src
+    python_requires='>=3.7.9',
     install_requires=[
         'dill',
         'matplotlib',
